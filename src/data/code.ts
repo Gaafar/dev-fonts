714344b29b31c0b74fc4a73fb2ac295d219265a4
Mostafa Gaafar <mgaafar@live.com>
2020-04-12 07:52:03 +0100

    update fonts

--- src/data/code.ts
+++ src/data/code.ts
@@ -4,7 +4,7 @@ export const code = `fonts.forEach(font => {
   const { id, name, rating } = font;
 
   if (
-    rating >= 4
+    rating >= 3
     || (rating === 5 && rating != 0)
     || popular[id]
   ) {
--- src/data/fonts.ts
+++ src/data/fonts.ts
@@ -2,12 +2,12 @@ import { orderBy } from 'lodash';
 
 const googleFonts = [
   { name: 'Fira Code', ligatures: true, sort: 1 },
-  { name: 'Roboto Mono', sort: 2 },
-  { name: 'Inconsolata', sort: 2 },
-  { name: 'Source Code Pro', sort: 2 },
-  { name: 'Anonymous Pro', sort: 2 },
-  { name: 'Space Mono', sort: 2 },
-  { name: 'Oxygen Mono', sort: 2 },
+  { name: 'Roboto Mono', sort: 3 },
+  { name: 'Inconsolata', sort: 3 },
+  { name: 'Source Code Pro', sort: 3 },
+  { name: 'Anonymous Pro', sort: 3 },
+  { name: 'Space Mono', sort: 3 },
+  { name: 'Oxygen Mono', sort: 3 },
   { name: 'B612 Mono' },
   { name: 'PT Mono' },
   { name: 'Nanum Gothic Coding' },
@@ -16,14 +16,46 @@ const googleFonts = [
   { name: 'Share Tech Mono' },
   // { name: 'VT323' },
   { name: 'Cousine' },
-  { name: 'Fira Mono' },
+  // { name: 'Fira Mono' },
   { name: 'Overpass Mono' },
   { name: 'Courier Prime' },
-  { name: 'Cutive Mono' },
+  // { name: 'Cutive Mono' },
   { name: 'Nova Mono' },
 ];
 
 const rawFonts = [
+  {
+    displayName: 'Cascadia Code',
+    familyName: 'Cascadia',
+    sort: 1,
+    ligatures: true,
+    webPage: 'https://github.com/microsoft/cascadia-code',
+    srcLink: '/fonts/cascadia/font.css',
+  },
+  {
+    displayName: 'JetBrains Mono',
+    familyName: 'JetBrains Mono',
+    sort: 2,
+    ligatures: true,
+    webPage: 'https://www.jetbrains.com/lp/mono/',
+    srcLink: '/fonts/jetbrains-mono/font.css',
+  },
+  {
+    displayName: 'Monoid',
+    familyName: 'Monoid Regular',
+    sort: 2,
+    ligatures: true,
+    webPage: 'http://larsenwork.com/monoid/',
+    srcLink: '/fonts/monoid/font.css',
+  },
+  {
+    displayName: 'Lilex',
+    familyName: 'Lilex',
+    sort: 2,
+    ligatures: true,
+    webPage: 'https://github.com/mishamyrt/Lilex',
+    srcLink: '/fonts/lilex/font.css',
+  },
   {
     displayName: 'Victor Mono',
     familyName: 'Victor Mono',
@@ -33,6 +65,30 @@ const rawFonts = [
     srcLink: 'https://cdn.jsdelivr.net/npm/victormono@latest/dist/index.min.css',
   },
   {
+    displayName: 'Iosevka',
+    familyName: 'Iosevka Web',
+    sort: 2,
+    ligatures: true,
+    webPage: 'https://typeof.net/Iosevka/',
+    srcLink: '/fonts/iosevka/font.css',
+  },
+  {
+    displayName: 'Iosevka Slab',
+    familyName: 'Iosevka Slab Web',
+    sort: 2,
+    ligatures: true,
+    webPage: 'https://typeof.net/Iosevka/',
+    srcLink: '/fonts/iosevka-slab/font.css',
+  },
+  {
+    displayName: 'Hasklig',
+    familyName: 'Hasklig',
+    sort: 2,
+    ligatures: true,
+    webPage: 'https://github.com/i-tu/Hasklig',
+    srcLink: '/fonts/hasklig/font.css',
+  },
+  {
     displayName: 'Noto Mono',
     familyName: 'TypoPRO Noto Mono',
     sort: 3,
@@ -40,14 +96,12 @@ const rawFonts = [
     srcLink: 'https://cdn.jsdelivr.net/npm/@typopro/web-noto@3.7.5/TypoPRO-Noto.css',
   },
   {
-    displayName: 'Monoid',
-    familyName: 'Monoid Regular',
-    sort: 2,
-    ligatures: true,
-    webPage: 'http://larsenwork.com/monoid/',
-    srcLink: '/fonts/monoid/monoid.css',
+    displayName: 'Conta',
+    familyName: 'Conta',
+    sort: 3,
+    webPage: 'https://fontesk.com/conta-typeface/',
+    srcLink: '/fonts/conta/font.css',
   },
-
   ...googleFonts.map((font) => {
     const { name, ligatures } = font;
     const urlPart = name.replace(/\s/g, '+');
