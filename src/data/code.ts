91a7de1de37b9b4184dde3b92b9ca7e6325406ce
Mostafa Gaafar <mgaafar@live.com>
2020-04-12 06:34:22 +0100

    update code snippet

--- src/data/code.ts
+++ src/data/code.ts
@@ -1,12 +1,13 @@
-import { fonts } from './fonts';
-
 // show brackets, braces, parens, equality ligs, && ||, arrow, gte
 
-export const code = `fonts.forEach(({ id, name, rating }) => {
-  if (favorite[id]) {
-    console.log(\`favorite font: \${name}\`);
-  }
-  if (rating >= 4 || (rating == 5 && rating !== 0)) {
-    console.log('popular font');
+export const code = `fonts.forEach(font => {
+  const { id, name, rating } = font;
+
+  if (
+    rating >= 4
+    || (rating === 5 && rating != 0)
+    || popular[id]
+  ) {
+    console.log(\`\${name} is popular\`);
   }
 });`;
